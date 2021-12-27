b12f6fedca81cd04942f8b000e3e4818607fe9b9
Peter6Q <luk10099@connect.hku.hk>
2021-12-27 17:37:09 +0800

    refactor: Instruction __post_init__

--- src/flashcardWorker/flashcardSystem/Instruction.py
+++ src/flashcardWorker/flashcardSystem/Instruction.py
@@ -3,6 +3,7 @@ import enum
 from dataclasses import dataclass, InitVar
 from enum import Enum
 from os import path
+from typing import ClassVar
 
 
 #### #### #### #### #### 
@@ -44,6 +45,17 @@ class InstructionType(Enum):
 
 @dataclass
 class Instruction:
+    StringToTypeMap:ClassVar[dict] = {
+        "add": InstructionType.ADD,
+        "del": InstructionType.DELETE,
+        "pri": InstructionType.CHANGE_FLASHCARD_PRIORITY,
+        "re": InstructionType.RESPOND_TO_QUESTION,
+        "freq": InstructionType.CHANGE_FLASHCARD_SHOWING_FREQUENCY,
+        "show": InstructionType.SHOW_FLASHCARD,
+        "info": InstructionType.SHOW_INFO,
+        "time": InstructionType.CHANGE_TIME_PRIORITY
+    }
+
     Type:InstructionType = InstructionType.UNKNOWN
     Id:int = None
     Key:str = ""
@@ -53,6 +65,8 @@ class Instruction:
     
     
     def __post_init__(self, text:str) -> None:
+        ## Variables initialization
+        cls = type(self)
         ## Pre-condition
         if not isinstance(text, str):
             self.Type = InstructionType.UNKNOWN
@@ -60,47 +74,66 @@ class Instruction:
         
         ## Main
         textSplitted = text.split(";")
+        #### To allow the use of ; in textSplitted[3]
         if len(textSplitted) > 4:
             textSplitted[3] = ";".join(textSplitted[3:] )
             del textSplitted[4:]
+        #### Remove leading and trailing whitespaces
         textSplitted = [t.strip() for t in textSplitted]
-        
+        #### Handle exceptional case
+        if not len(textSplitted):
+            log.error("Should be unreachable!")
+            self.Type = InstructionType.UNKNOWN
+            return
+        #### Get the instruction type from textSplitted[0]
+        userInputTypeStr = textSplitted[0].lower()
+        self.Type = cls.StringToTypeMap.get(userInputTypeStr, 
+            InstructionType.UNKNOWN)
+        #### Case when len(textSplitted) == 1
         if len(textSplitted) == 1:
-            if textSplitted[0] == "info":
-                self.Type = InstructionType.SHOW_INFO
-            else:
+            if not self.Type == InstructionType.SHOW_INFO:
                 self.Type = InstructionType.UNKNOWN
             return
-        
-        if textSplitted[0] == "add":
-            self.Type = InstructionType.ADD
+        #### Case when len(textSplitted) in range [2, 4]
+        if self.Type == InstructionType.ADD:
             self.Key = textSplitted[1]
             self.Value = textSplitted[2] if len(textSplitted) >= 3 else ""
             self.Remarks = textSplitted[3] if len(textSplitted) == 4 else ""
-        elif textSplitted[0] == "del":
-            self.Type = InstructionType.DELETE
+        elif self.Type == InstructionType.DELETE:
             self.Key = textSplitted[1]
-        elif textSplitted[0] == "pri":
-            self.Type = InstructionType.CHANGE_FLASHCARD_PRIORITY
+        elif self.Type == InstructionType.CHANGE_FLASHCARD_PRIORITY:
             self.Key = textSplitted[1]
             self.Value = textSplitted[2] if len(textSplitted) >= 3 else "1"
-        elif textSplitted[0] == "re":
-            self.Type = InstructionType.RESPOND_TO_QUESTION
+        elif self.Type == InstructionType.RESPOND_TO_QUESTION:
             self.Value = textSplitted[1]
-        elif textSplitted[0] == "freq":
-            self.Type = InstructionType.CHANGE_FLASHCARD_SHOWING_FREQUENCY
+        elif self.Type == InstructionType.CHANGE_FLASHCARD_SHOWING_FREQUENCY:
             self.Value = textSplitted[1]
-        elif textSplitted[0] == "show":
-            self.Type = InstructionType.SHOW_FLASHCARD
+        elif self.Type == InstructionType.SHOW_FLASHCARD:
             self.Key = textSplitted[1]
-        elif textSplitted[0] == "time":
-            self.Type = InstructionType.CHANGE_TIME_PRIORITY
+        elif self.Type == InstructionType.CHANGE_TIME_PRIORITY:
             self.Key = textSplitted[1]
             self.Value = textSplitted[2] if len(textSplitted) >= 3 else "1"
     
     
-if __name__ == "__main__":
-    x = Instruction(Id=1, text="add;x;Explanation of x;Remarks of x")
+if __name__ == "__main__":    
+    x = Instruction(text="add;x;x explanation;x remarks")
     print(x)
-    y = Instruction(Id=1, text="Add;y;Explanation of y;Remarks of y")
-    print(y)
+    x = Instruction(text="del;x;x explanation;x remarks")
+    print(x)
+    x = Instruction(text="pri;x;x explanation;x remarks")
+    print(x)
+    x = Instruction(text="re;x;x explanation;x remarks")
+    print(x)
+    x = Instruction(text="freq;x;x explanation;x remarks")
+    print(x)
+    x = Instruction(text="show;x;x explanation;x remarks")
+    print(x)
+    x = Instruction(text="info;x;x explanation;x remarks")
+    print(x)
+    x = Instruction(text="time;x;x explanation;x remarks")
+    print(x)
+    
+    x = Instruction(Id=1, text=" add  ;  x ; x  explanation;x  remarks   ")
+    print(x)
+    x = Instruction(Id=1, text="hi;x;x  explanation;x remarks")
+    print(x)
